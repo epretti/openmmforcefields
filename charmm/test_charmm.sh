c41ed5ef4db9d1a79532ba8c4bb6b4ba26ae4798
Evan Pretti <pretti@stanford.edu>
2025-02-27 15:43:00 -0800

    Split CHARMM test into its own CI workflow

--- charmm/test_charmm.sh
+++ charmm/test_charmm.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-set -evx
-
-python test_charmm.py tests/01*/*.yaml --charmm --openmm-charmm --openmm-ffxml --openmm-ffxml-fix-impropers --perturb-replicates 5 --perturb-seed 291700478 "$@"
+python test_charmm.py tests/01*/*.yaml --charmm --openmm-charmm --openmm-ffxml-fix-impropers --perturb-replicates 5 --perturb-seed 291700478 "$@"
