5b1897b8fbe876929259e49562ec1ca486ed66eb
Evan Pretti <pretti@stanford.edu>
2025-02-27 16:36:57 -0800

    Cleanup and reduce number of replicates

--- charmm/test_charmm.sh
+++ charmm/test_charmm.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-python test_charmm.py tests/16*/*.yaml --charmm --openmm-charmm --openmm-ffxml-fix-impropers --perturb-replicates 5 --perturb-seed 291700478 "$@"
+# TODO: want to test with --openmm-ffxml once OpenMM is fixed
+python test_charmm.py tests/*/*.yaml --charmm --openmm-charmm --openmm-ffxml-fix-impropers --perturb-replicates 3 --perturb-seed 291700478 "$@"
