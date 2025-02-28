80ba9ada1e46de2480c73164b26a57a519ea93c6
Evan Pretti <pretti@stanford.edu>
2025-02-27 16:28:45 -0800

    Debugging POPC test

--- charmm/test_charmm.sh
+++ charmm/test_charmm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python test_charmm.py tests/*/*.yaml --charmm --openmm-charmm --openmm-ffxml-fix-impropers --perturb-replicates 5 --perturb-seed 291700478 "$@"
+python test_charmm.py tests/16*/*.yaml --charmm --openmm-charmm --openmm-ffxml-fix-impropers --perturb-replicates 5 --perturb-seed 291700478 "$@"
